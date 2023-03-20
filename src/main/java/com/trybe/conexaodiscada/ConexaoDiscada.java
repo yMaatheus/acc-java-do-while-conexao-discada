4b978370db62cfb99ff8df1cccbca20ff522e022
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:19:01 -0300

    Initial commit

---
+++ src/main/java/com/trybe/conexaodiscada/ConexaoDiscada.java
@@ -0,0 +1,31 @@
+package com.trybe.conexaodiscada;
+
+import java.util.Random;
+
+/**
+ * Classe ConexaoDiscada.
+ *
+ */
+public class ConexaoDiscada {
+
+  /**
+   * Método principal.
+   *
+   */
+  public static void main(String[] args) {
+    // Seu código aqui
+  }
+
+  // Chame esta função escrevendo `conectou()`
+  static boolean conectou() {
+    return conexaoDiscada.verdadeiroOuFalso();
+  }
+
+  static ConexaoDiscada conexaoDiscada = new ConexaoDiscada();
+
+  // Retorna verdadeiro ou falso
+  boolean verdadeiroOuFalso() {
+    return new Random().nextInt(2) == 1;
+  }
+
+}
